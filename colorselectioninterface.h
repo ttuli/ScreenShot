3c8cf4be83bd114d34f3fdd76380df9ca81f235c
ttuli <koung123@126.com>
2024-12-15 15:07:17 +0800

    11

--- colorselectioninterface.h
+++ colorselectioninterface.h
@@ -2,12 +2,21 @@
 #define COLORSELECTIONINTERFACE_H
 
 #include <QWidget>
+#include <QHBoxLayout>
 
 class ColorSelectionInterface : public QWidget
 {
     Q_OBJECT
 public:
     explicit ColorSelectionInterface(QWidget *parent = nullptr);
+
+
+private:
+    QHBoxLayout *qhb;
+
+    QWidget *size1;
+    QWidget *size2;
+    QWidget *size3;
 
 signals:
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "widget.h"
-#include "settingdia.h"
+#include "colorselectioninterface.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/new/prefix1/res/Scissors.svg"));
-    a.setQuitOnLastWindowClosed(0);
-    Widget w;
+    //a.setQuitOnLastWindowClosed(0);
+    ColorSelectionInterface w;
+    w.show();
     return a.exec();
 }
